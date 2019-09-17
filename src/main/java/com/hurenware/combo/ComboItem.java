06870ac834747f6a49e82a0d425af8d7aca1cd4c
Victor do Prado <victorprado123@gmail.com>
2019-09-17 14:48:26 -0300

    Projeto Calendário TPA

---
+++ src/main/java/com/hurenware/combo/ComboItem.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hurenware.combo;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ComboItem {
+
+    private int value;
+    private String label;
+
+    public ComboItem(int value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
+}
---
+++ src/main/java/com/hurenware/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hurenware.main;
+import com.hurenware.tela.*;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    Principal tela = new Principal();
+    tela.setVisible(true);
+    }
+}
